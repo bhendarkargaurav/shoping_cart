18236f519efbd9df5bf6515eb87a9ab8dd82d1b3
Gaurav <gaurav.bhe.24@gmail.com>
2026-01-28 18:24:22 +0530

    card page

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "1",
+    name: "Casual Shirt",
+    price: 45,
+    img: "images/img-1.jpg",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
+  },
+  {
+    id: "2",
+    name: "Official Shirt",
+    price: 55,
+    img: "images/img-4.jpg",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
+  },
+  {
+    id: "3",
+    name: "Party Clothes",
+    price: 90,
+    img: "images/img-2.jpg",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
+  },
+  {
+    id: "4",
+    name: "Combo Pack",
+    price: 70,
+    img: "images/img-3.jpg",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -123,36 +123,7 @@
 
 let shop = document.getElementById("shop");
 
-let shopItemsData = [
-  {
-    id: "1",
-    name: "Casual Shirt",
-    price: 45,
-    img: "images/img-1.jpg",
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
-  },
-  {
-    id: "2",
-    name: "Official Shirt",
-    price: 55,
-    img: "images/img-4.jpg",
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
-  },
-  {
-    id: "3",
-    name: "Party Clothes",
-    price: 90,
-    img: "images/img-2.jpg",
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
-  },
-  {
-    id: "4",
-    name: "Combo Pack",
-    price: 70,
-    img: "images/img-3.jpg",
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing",
-  },
-];
+
 
 // Cart state
 let basket = JSON.parse(localStorage.getItem("data")) || [];
@@ -174,7 +145,9 @@ let generateShop = () => {
               <h2>$ ${price}</h2>
               <div class="buttons">
                 <i onclick="decrement('${id}')" class="bi bi-dash-lg"></i>
-                <div id="${id}" class="quantity">${search.item}</div>
+                <div id="${id}" class="quantity">
+                ${search.item === undefined ? 0 : search.item }
+                </div>
                 <i onclick="increment('${id}')" class="bi bi-plus-lg"></i>
               </div>
             </div>
@@ -201,21 +174,6 @@ let increment = (id) => {
 
   localStorage.setItem("data", JSON.stringify(basket));
 };
-
-//Decrement
-// let decrement = (id) => {
-//   let search = basket.find((x) => x.id === id);
-//   if (!search) return;
-
-//   search.item -= 1;
-
-//   if (search.item === 0) {
-//     basket = basket.filter((x) => x.id !== id);
-//   }
-
-//   localStorage.setItem("data", JSON.stringify(basket));
-//   update(id);
-// };
 
 
 
